21628d28595d84b531372ade312406f099984d5e
Taichi Sasaki <t.sasaki178@gmail.com>
2020-08-18 08:02:27 +0000

    ウィジェット分割

---
+++ lib/ui/parts/ok_button.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tcg_recorder/entity/record.dart';
+import 'package:tcg_recorder/model/deck_model.dart';
+import 'package:tcg_recorder/model/game_model.dart';
+import 'package:tcg_recorder/model/record_model.dart';
+import 'package:tcg_recorder/model/tag_model.dart';
+
+class OkButton extends StatelessWidget {
+  const OkButton({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
+    final _deckModel = Provider.of<DeckModel>(context, listen: false);
+    final _selectedGame =
+        context.select((GameModel model) => model.selectedGame);
+    final _selectedTag = context.select((TagModel model) => model.selectedTag);
+    final _selectedUseDeck =
+        context.select((DeckModel model) => model.selectedUseDeck);
+    final _selectedOpponentDeck =
+        context.select((DeckModel model) => model.selectedOpponentDeck);
+    final _selectedFirstOrSecond =
+        context.select((RecordModel model) => model.firstOrSecond);
+    final _selectedWinOrLose =
+        context.select((RecordModel model) => model.winOrLose);
+
+    if (_selectedGame != null) {
+      _deckModel.getGameDeckList(_selectedGame.gameId);
+    }
+    return Container(
+      height: _size.height * (10 / 100),
+      width: _size.width * (80 / 100),
+      padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
+      child: RaisedButton(
+        onPressed: _selectedGame == null ||
+                _selectedUseDeck == null ||
+                _selectedOpponentDeck == null
+            ? null
+            : () async {
+                if (_selectedGame.gameId == 0)
+                  await context.read<GameModel>().test();
+                if (_selectedTag.tagId == 0)
+                  await context.read<TagModel>().test();
+                if (_selectedUseDeck.deckId == 0)
+                  await context.read<DeckModel>().testUse();
+                if (_selectedOpponentDeck.deckId == 0)
+                  await context.read<DeckModel>().testOpponent();
+                await context.read<RecordModel>().add(
+                      Record(
+                        date: DateTime.now(),
+                        gameId: _selectedGame.gameId,
+                        tagId: _selectedTag == null ? null : _selectedTag.tagId,
+                        myDeckId: _selectedUseDeck.deckId,
+                        opponentDeckId: _selectedOpponentDeck.deckId,
+                        firstOrSecond: _selectedFirstOrSecond,
+                        winOrLose: _selectedWinOrLose,
+                        memo: null,
+                      ),
+                    );
+                context.read<TagModel>().selectedTagChangeToString('');
+                context.read<DeckModel>().selectedUseDeckChangeToString('');
+                context
+                    .read<DeckModel>()
+                    .selectedOpponentDeckChangeToString('');
+              },
+        child: const Text('OK'),
+      ),
+    );
+  }
+}
