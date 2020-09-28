5643bdd6f590b0e87a91e77a9df56fc0f366cfbe
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2020-09-28 09:19:59 +0900

    設定画面削除

--- lib/model/bottom_navigation_model.dart
+++ lib/model/bottom_navigation_model.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:tcg_recorder/ui/game_list_screen.dart';
 import 'package:tcg_recorder/ui/graph_list_screen.dart';
 import 'package:tcg_recorder/ui/input_screen.dart';
-import 'package:tcg_recorder/ui/setting_screen.dart';
+// import 'package:tcg_recorder/ui/setting_screen.dart';
 
 class BottomNavigationModel with ChangeNotifier {
   final List<Widget> options = [
     const InputScreen(),
     const GameListScreen(),
     const GraphListScreen(),
-    const SettingScreen(),
+    // const SettingScreen(),
   ];
 
   int selectedIndex = 0;
