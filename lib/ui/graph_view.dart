d6e69baa60ab3e7b4687abd0be061dd6243aa97f
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2020-10-09 13:08:04 +0900

    データが無いときにない旨を表示するように変更

--- lib/ui/graph_view.dart
+++ lib/ui/graph_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tcg_recorder/localization/l10n.dart';
+import 'package:tcg_recorder/model/graph_model.dart';
 import 'package:tcg_recorder/ui/parts/data_screen/opponent_deck_percentage_graph.dart';
 import 'package:tcg_recorder/ui/parts/data_screen/use_deck_detail.dart';
 import 'package:tcg_recorder/ui/parts/data_screen/use_deck_percentage_graph.dart';
@@ -50,15 +52,17 @@ class _UseageRateView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: const [
-          UseDeckPercentageGraph(),
-          _Divider(),
-          OpponentDeckPercentageGraph(),
-        ],
-      ),
-    );
+    return context.select((GraphModel model) => model.deckList).isEmpty
+        ? Center(child: Text(L10n.of(context).noItem))
+        : SingleChildScrollView(
+            child: Column(
+              children: const [
+                UseDeckPercentageGraph(),
+                _Divider(),
+                OpponentDeckPercentageGraph(),
+              ],
+            ),
+          );
   }
 }
 
@@ -69,15 +73,17 @@ class _WinRateView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: const [
-          WinRateGraph(),
-          _Divider(),
-          UseDeckDetail(),
-        ],
-      ),
-    );
+    return context.select((GraphModel model) => model.deckList).isEmpty
+        ? Center(child: Text(L10n.of(context).noItem))
+        : SingleChildScrollView(
+            child: Column(
+              children: const [
+                WinRateGraph(),
+                _Divider(),
+                UseDeckDetail(),
+              ],
+            ),
+          );
   }
 }
 
