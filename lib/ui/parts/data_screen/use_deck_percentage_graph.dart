b6865ecce26c3b1e76efd83da4e9aeb9d8c91d33
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2020-12-03 12:46:33 +0900

    円グラフのウィジェットを分割

--- lib/ui/parts/data_screen/use_deck_percentage_graph.dart
+++ lib/ui/parts/data_screen/use_deck_percentage_graph.dart
@@ -3,6 +3,7 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:provider/provider.dart';
 import 'package:tcg_recorder/localization/l10n.dart';
 import 'package:tcg_recorder/model/graph_model.dart';
+import 'package:tcg_recorder/ui/parts/pie_chart.dart';
 
 class UseDeckPercentageGraph extends StatelessWidget {
   const UseDeckPercentageGraph({key}) : super(key: key);
@@ -12,38 +13,19 @@ class UseDeckPercentageGraph extends StatelessWidget {
     final model = context.watch<GraphModel>();
     return Padding(
       padding: const EdgeInsets.only(top: 12, left: 8, right: 8),
-      child: Card(
-        color: Theme.of(context).cardColor,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        child: Container(
-          width: 400,
-          height: 280,
-          child: SfCircularChart(
-            title: ChartTitle(
-              text: L10n.of(context).useDeckPercentageGraphTitle,
-            ),
-            legend: Legend(
-              isVisible: true,
-              position: LegendPosition.right,
-              overflowMode: LegendItemOverflowMode.scroll,
-              width: '40%',
-            ),
-            tooltipBehavior: TooltipBehavior(enable: true),
-            series: <CircularSeries>[
-              PieSeries<DeckDetailData, String>(
-                enableTooltip: true,
-                dataSource: model.useDeckGraphDetailList,
-                pointColorMapper: (DeckDetailData data, _) => data.color,
-                xValueMapper: (DeckDetailData data, _) => data.deck.deck,
-                yValueMapper: (DeckDetailData data, _) => data.useageRate,
-                animationDuration: 0,
-                radius: '100%',
-              )
-            ],
-          ),
-        ),
+      child: PieChart(
+        title: L10n.of(context).useDeckPercentageGraphTitle,
+        series: <CircularSeries>[
+          PieSeries<DeckDetailData, String>(
+            enableTooltip: true,
+            dataSource: model.useDeckGraphDetailList,
+            pointColorMapper: (DeckDetailData data, _) => data.color,
+            xValueMapper: (DeckDetailData data, _) => data.deck.deck,
+            yValueMapper: (DeckDetailData data, _) => data.useageRate,
+            animationDuration: 0,
+            radius: '90%',
+          )
+        ],
       ),
     );
   }
---
+++ lib/ui/parts/pie_chart.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class PieChart extends StatelessWidget {
+  const PieChart({
+    @required this.series,
+    this.radius = 10,
+    this.width = 400,
+    this.height = 280,
+    this.title = '',
+    this.titleAlignment = ChartAlignment.near,
+    this.titleStyle,
+    this.legendIsVisible = true,
+    this.legendPosition = LegendPosition.right,
+    this.legendOverFlowMode = LegendItemOverflowMode.wrap,
+    this.tooltipBehavior = true,
+    key,
+  }) : super(key: key);
+
+  final List<CircularSeries<dynamic, dynamic>> series;
+  final double radius;
+  final double width;
+  final double height;
+  final String title;
+  final ChartAlignment titleAlignment;
+  final TextStyle titleStyle;
+  final bool legendIsVisible;
+  final LegendPosition legendPosition;
+  final LegendItemOverflowMode legendOverFlowMode;
+  final bool tooltipBehavior;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).cardColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(radius)),
+      ),
+      child: Container(
+        width: width,
+        height: height,
+        child: SfCircularChart(
+          title: ChartTitle(
+            text: title,
+            alignment: titleAlignment,
+            textStyle: titleStyle ??
+                const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          legend: Legend(
+            isVisible: legendIsVisible,
+            position: legendPosition,
+            overflowMode: legendOverFlowMode,
+          ),
+          tooltipBehavior: TooltipBehavior(enable: tooltipBehavior),
+          series: series,
+        ),
+      ),
+    );
+  }
+}
