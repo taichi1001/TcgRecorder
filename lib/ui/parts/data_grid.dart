8048e8da0e0f6e08e5b6e88163cd06effde9b2b3
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2020-12-03 17:07:57 +0900

    widget分割

---
+++ lib/ui/parts/data_grid.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
+
+class DataGrid extends StatelessWidget {
+  const DataGrid({
+    @required this.source,
+    @required this.columns,
+    this.radius = 10,
+    this.width = 400,
+    this.height = 280,
+    this.gridLineStrokeWidth = 0.5,
+    this.headerBGColor,
+    this.headerTextStyle,
+    this.footerFrozenRowsCount,
+    this.cellBuilder,
+    key,
+  }) : super(key: key);
+
+  final DataGridSource<Object> source;
+  final List<GridColumn> columns;
+  final double radius;
+  final double width;
+  final double height;
+  final double gridLineStrokeWidth;
+  final Color headerBGColor;
+  final TextStyle headerTextStyle;
+  final int footerFrozenRowsCount;
+  final Widget Function(BuildContext, GridColumn, int) cellBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).cardColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(radius)),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(radius)),
+        child: Container(
+          height: height,
+          // width: width,
+          child: SfDataGridTheme(
+            data: SfDataGridThemeData(
+              gridLineStrokeWidth: gridLineStrokeWidth,
+              headerStyle: DataGridHeaderCellStyle(
+                backgroundColor: headerBGColor ?? Theme.of(context).buttonColor,
+                textStyle: headerTextStyle ??
+                    const TextStyle(
+                      color: Colors.white,
+                    ),
+              ),
+            ),
+            child: SfDataGrid(
+              source: source,
+              cellBuilder: cellBuilder,
+              footerFrozenRowsCount: footerFrozenRowsCount,
+              columns: columns,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/parts/data_screen/use_deck_detail.dart
+++ lib/ui/parts/data_screen/use_deck_detail.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
-import 'package:syncfusion_flutter_core/theme.dart';
 import 'package:provider/provider.dart';
 import 'package:tcg_recorder/model/graph_model.dart';
 import 'package:tcg_recorder/ui/deck_detail_screen.dart';
+import 'package:tcg_recorder/ui/parts/data_grid.dart';
 
 class UseDeckDetail extends StatelessWidget {
   const UseDeckDetail({key}) : super(key: key);
@@ -15,91 +15,66 @@ class UseDeckDetail extends StatelessWidget {
     return Padding(
       padding:
           const EdgeInsets.only(top: 12, left: 8, right: 8), //cardはデフォルトで4のマージンがあるため、２つ目以降は16ではなく12
-      child: Card(
-        color: Theme.of(context).cardColor,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          child: Container(
-            height: 350,
-            child: SfDataGridTheme(
-              data: SfDataGridThemeData(
-                gridLineStrokeWidth: 0.5,
-                headerStyle: DataGridHeaderCellStyle(
-                  backgroundColor: Theme.of(context).buttonColor,
-                  textStyle: const TextStyle(
-                    color: Colors.white,
+      child: DataGrid(
+        source: model.useDeckDetailDataGridSource,
+        cellBuilder: (context, gridColumn, rowIndex) =>
+            model.useDeckDetailList[rowIndex].deck.deck != '合計'
+                ? FlatButton(
+                    onPressed: () {
+                      model.selectedDeck = model.useDeckDetailList[rowIndex].deck;
+                      model.makeVsDeckDetailList();
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => VsDeckDetailScreen(
+                            deck: model.useDeckDetailList[rowIndex].deck,
+                            model: model,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: _size.width * (35 / 100),
+                      child: Text(
+                        model.useDeckDetailList[rowIndex].deck.deck,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  )
+                : Center(
+                    child: Container(
+                      width: _size.width * (35 / 100),
+                      padding: const EdgeInsets.only(right: 20, left: 20),
+                      child: Text(
+                        model.useDeckDetailList[rowIndex].deck.deck,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              child: SfDataGrid(
-                source: model.useDeckDetailDataGridSource,
-                cellBuilder: (BuildContext context, GridColumn column, int rowIndex) {
-                  return model.useDeckDetailList[rowIndex].deck.deck != '合計'
-                      ? FlatButton(
-                          onPressed: () {
-                            model.selectedDeck = model.useDeckDetailList[rowIndex].deck;
-                            model.makeVsDeckDetailList();
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => VsDeckDetailScreen(
-                                  deck: model.useDeckDetailList[rowIndex].deck,
-                                  model: model,
-                                ),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            width: _size.width * (35 / 100),
-                            child: Text(
-                              model.useDeckDetailList[rowIndex].deck.deck,
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                        )
-                      : Center(
-                          child: Container(
-                            width: _size.width * (35 / 100),
-                            padding: const EdgeInsets.only(right: 20, left: 20),
-                            child: Text(
-                              model.useDeckDetailList[rowIndex].deck.deck,
-                              textAlign: TextAlign.left,
-                            ),
-                          ),
-                        );
-                },
-                footerFrozenRowsCount: 1,
-                columns: [
-                  GridWidgetColumn(mappingName: 'deck', headerText: 'デッキ名')
-                    ..textAlignment = Alignment.centerLeft
-                    ..headerTextAlignment = Alignment.centerLeft
-                    ..softWrap = true
-                    ..headerTextSoftWrap = true
-                    ..width = _size.width * (30 / 100),
-                  GridNumericColumn(mappingName: 'matches', headerText: '試合')
-                    ..width = _size.width * (15 / 100)
-                    ..headerTextAlignment = Alignment.center
-                    ..textAlignment = Alignment.center
-                    ..headerTextOverflow = TextOverflow.visible,
-                  GridTextColumn(mappingName: 'win', headerText: '勝')
-                    ..width = _size.width * (15 / 100)
-                    ..headerTextAlignment = Alignment.center
-                    ..textAlignment = Alignment.center,
-                  GridTextColumn(mappingName: 'lose', headerText: '負')
-                    ..width = _size.width * (15 / 100)
-                    ..headerTextAlignment = Alignment.center
-                    ..textAlignment = Alignment.center,
-                  GridNumericColumn(mappingName: 'winRate', headerText: '勝率')
-                    ..width = _size.width * (20 / 100)
-                    ..headerTextAlignment = Alignment.center
-                    ..textAlignment = Alignment.center,
-                ],
-              ),
-            ),
-          ),
-        ),
+        columns: [
+          GridWidgetColumn(mappingName: 'deck', headerText: 'デッキ名')
+            ..textAlignment = Alignment.centerLeft
+            ..headerTextAlignment = Alignment.centerLeft
+            ..softWrap = true
+            ..headerTextSoftWrap = true
+            ..width = _size.width * (30 / 100),
+          GridNumericColumn(mappingName: 'matches', headerText: '試合')
+            ..width = _size.width * (15 / 100)
+            ..headerTextAlignment = Alignment.center
+            ..textAlignment = Alignment.center
+            ..headerTextOverflow = TextOverflow.visible,
+          GridTextColumn(mappingName: 'win', headerText: '勝')
+            ..width = _size.width * (15 / 100)
+            ..headerTextAlignment = Alignment.center
+            ..textAlignment = Alignment.center,
+          GridTextColumn(mappingName: 'lose', headerText: '負')
+            ..width = _size.width * (15 / 100)
+            ..headerTextAlignment = Alignment.center
+            ..textAlignment = Alignment.center,
+          GridNumericColumn(mappingName: 'winRate', headerText: '勝率')
+            ..width = _size.width * (20 / 100)
+            ..headerTextAlignment = Alignment.center
+            ..textAlignment = Alignment.center,
+        ],
       ),
     );
   }
