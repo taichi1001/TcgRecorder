21180e7bff7c53f0be77d221493b3512a67a848e
taichi1001 <t.sasaki178@gmail.com>
2022-01-30 01:14:46 +0900

    datePicker日本語化、MaterialAppを共通化

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -13,8 +15,8 @@ import 'package:tcg_recorder2/view/initial_game_registration_view.dart';
 void main() {
   runApp(
     DevicePreview(
-      // enabled: !kReleaseMode,
-      enabled: false,
+      enabled: !kReleaseMode,
+      // enabled: false,
       builder: (context) => const ProviderScope(
         child: MainApp(),
       ),
@@ -34,33 +36,34 @@ class MainApp extends HookConsumerWidget {
       Future.microtask(() => ref.read(allTagListNotifierProvider.notifier).fetch());
       return;
     }, const []);
-    final gameListState = ref.watch(allGameListNotifierProvider);
-    final deckListState = ref.watch(allDeckListNotifierProvider);
-    final recordListState = ref.watch(allRecordListNotifierProvider);
-    final tagListState = ref.watch(allTagListNotifierProvider);
+    final allGameList = ref.watch(allGameListNotifierProvider).allGameList;
+    final allDeckList = ref.watch(allDeckListNotifierProvider).allDeckList;
+    final allRecordList = ref.watch(allRecordListNotifierProvider).allRecordList;
+    final allTagList = ref.watch(allTagListNotifierProvider).allTagList;
 
-    if (gameListState.allGameList == null &&
-        deckListState.allDeckList == null &&
-        recordListState.allRecordList == null &&
-        tagListState.allTagList == null) {
-      return MaterialApp(
-        useInheritedMediaQuery: true,
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        home: const Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        ),
-      );
-    }
     return MaterialApp(
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      home: Scaffold(
-        body: gameListState.allGameList!.isEmpty ? const InitialGameRegistrationView() : const BottomNavigationView(),
-      ),
+      localizationsDelegates: const [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('ja', 'JP'),
+      ],
+      home: allGameList == null && allDeckList == null && allRecordList == null && allTagList == null
+          ? const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : allGameList!.isEmpty
+              ? const InitialGameRegistrationView()
+              : const BottomNavigationView(),
     );
   }
 }
