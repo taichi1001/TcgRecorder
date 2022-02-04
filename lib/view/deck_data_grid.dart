d7f3077254376c8500ce61c35bdc8379bbc507d0
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2022-02-04 09:21:09 +0900

    %の表示が改行されていたのを修正

--- lib/view/deck_data_grid.dart
+++ lib/view/deck_data_grid.dart
@@ -127,7 +127,7 @@ class DeckDataGrid extends HookConsumerWidget {
                         ),
                       ),
                     ),
-                    width: 80,
+                    width: 85,
                   ),
                 ],
               ),
