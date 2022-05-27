29c5db8c0345d0ec5066ca7dc917e1f0201f816a
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2022-05-27 10:54:52 +0900

    画面を縦に固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -22,6 +23,9 @@ import 'generated/l10n.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   ATT.instance.requestPermission().then((result) {
     MobileAds.instance.initialize();
   });
