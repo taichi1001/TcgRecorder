f0bd94113f93a3682aa504984ea4871cdbbbc571
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2022-06-06 13:24:51 +0900

    検索用textfield追加

--- lib/view/select_deck_view.dart
+++ lib/view/select_deck_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tcg_manager/entity/deck.dart';
 import 'package:tcg_manager/helper/convert_sort_string.dart';
@@ -14,26 +15,43 @@ class SelectDeckView extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final recentlyUseDeckList = ref.watch(recentlyUseDeckProvider);
     final gameDeckList = ref.watch(sortedRecordListProvider);
+    final searchTextController = useTextEditingController();
+    final searchFocusNode = useFocusNode();
+    final isSearch = useState(false);
+
+    searchFocusNode.addListener(() {
+      isSearch.value = searchFocusNode.hasFocus;
+    });
+
+    searchTextController.addListener(() {
+      print(searchTextController.text);
+    });
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ここが検索欄になる'),
+        title: TextField(
+          controller: searchTextController,
+          focusNode: searchFocusNode,
+        ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                '最近使用したデッキ',
-                style: Theme.of(context).textTheme.caption,
+        child: Visibility(
+          visible: !isSearch.value,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  '最近使用したデッキ',
+                  style: Theme.of(context).textTheme.caption,
+                ),
               ),
-            ),
-            _DeckListView(deckList: recentlyUseDeckList),
-            const _AllListViewTitle(),
-            _DeckListView(deckList: gameDeckList),
-          ],
+              _DeckListView(deckList: recentlyUseDeckList),
+              const _AllListViewTitle(),
+              _DeckListView(deckList: gameDeckList),
+            ],
+          ),
         ),
       ),
     );
@@ -47,21 +65,24 @@ class _AllListViewTitle extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final sort = ref.watch(selectDeckViewNotifierProvider.select((value) => value.sortType));
     final selectDeckViewNotifier = ref.watch(selectDeckViewNotifierProvider.notifier);
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          '一覧',
-          style: Theme.of(context).textTheme.caption,
-        ),
-        CupertinoButton(
-          onPressed: () => selectDeckViewNotifier.changeSort(),
-          child: Text(
-            ConvertSortString.convert(context, sort),
-            style: Theme.of(context).textTheme.bodyText2,
+    return Padding(
+      padding: const EdgeInsets.only(left: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '一覧',
+            style: Theme.of(context).textTheme.caption,
           ),
-        ),
-      ],
+          CupertinoButton(
+            onPressed: () => selectDeckViewNotifier.changeSort(),
+            child: Text(
+              ConvertSortString.convert(context, sort),
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
