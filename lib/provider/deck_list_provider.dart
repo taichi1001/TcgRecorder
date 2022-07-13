e56e71ed42d55ec6414982092e9952914bd59088
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2022-07-13 10:48:03 +0900

    冗長コード修正

--- lib/provider/deck_list_provider.dart
+++ lib/provider/deck_list_provider.dart
@@ -2,7 +2,4 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tcg_manager/entity/deck.dart';
 import 'package:tcg_manager/repository/deck_repository.dart';
 
-final allDeckListProvider = FutureProvider<List<Deck>>((ref) async {
-  final allDeckList = await ref.read(deckRepository).getAll();
-  return allDeckList;
-});
+final allDeckListProvider = FutureProvider<List<Deck>>((ref) async => await ref.read(deckRepository).getAll());
--- lib/provider/record_list_provider.dart
+++ lib/provider/record_list_provider.dart
@@ -2,7 +2,4 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tcg_manager/entity/record.dart';
 import 'package:tcg_manager/repository/record_repository.dart';
 
-final allRecordListProvider = FutureProvider<List<Record>>((ref) async {
-  final allRecordList = await ref.read(recordRepository).getAll();
-  return allRecordList;
-});
+final allRecordListProvider = FutureProvider<List<Record>>((ref) async => await ref.read(recordRepository).getAll());
