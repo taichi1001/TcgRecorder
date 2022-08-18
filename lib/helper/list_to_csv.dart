7be2a507e0d8ce6213a1d14b7a3e11d337e89624
taichi1001 <t.sasaki178@gmail.com>
2022-08-18 16:59:03 +0900

    csv出力機能追加

---
+++ lib/helper/list_to_csv.dart
@@ -0,0 +1,45 @@
+import 'package:tcg_manager/entity/marged_record.dart';
+import 'package:csv/csv.dart';
+
+class ListToCSV {
+  static String margeRecordListToCSV(List<MargedRecord> list) {
+    List<List<dynamic>> rows = [];
+    List<dynamic> row = [];
+    row.add('ゲーム');
+    row.add('タグ');
+    row.add('BO');
+    row.add('使用デッキ');
+    row.add('対戦相手デッキ');
+    row.add('日付');
+    row.add('先攻・後攻');
+    row.add('1試合目 先攻・後攻');
+    row.add('2試合目 先攻・後攻');
+    row.add('3試合目 先攻・後攻');
+    row.add('勝敗');
+    row.add('1試合目 勝敗');
+    row.add('2試合目 勝敗');
+    row.add('3試合目 勝敗');
+    row.add('メモ');
+    rows.add(row);
+    for (final margedRecord in list) {
+      row = [];
+      row.add(margedRecord.game);
+      row.add(margedRecord.tag);
+      row.add(margedRecord.bo.name);
+      row.add(margedRecord.useDeck);
+      row.add(margedRecord.opponentDeck);
+      row.add(margedRecord.date);
+      row.add(margedRecord.firstSecond.name);
+      row.add(margedRecord.firstMatchFirstSecond?.name);
+      row.add(margedRecord.secondMatchFirstSecond?.name);
+      row.add(margedRecord.thirdMatchFirstSecond?.name);
+      row.add(margedRecord.winLoss.name);
+      row.add(margedRecord.firstMatchWinLoss?.name);
+      row.add(margedRecord.secondMatchWinLoss?.name);
+      row.add(margedRecord.thirdMatchWinLoss?.name);
+      row.add(margedRecord.memo);
+      rows.add(row);
+    }
+    return const ListToCsvConverter().convert(rows);
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import in_app_review
 import macos_ui
 import path_provider_macos
 import purchases_flutter
+import share_plus_macos
 import shared_preferences_macos
 import sqflite
 import url_launcher_macos
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
