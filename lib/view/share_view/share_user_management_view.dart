e3d526d6d585f54cb631f25e8874a24de834da9f
Sasaki Taichi <taichi_sasaki@nidek.co.jp>
2023-08-24 17:43:59 +0900

    共有解除機能追加

--- lib/view/share_view/share_user_management_view.dart
+++ lib/view/share_view/share_user_management_view.dart
@@ -1,10 +1,26 @@
+import 'dart:io';
+
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:tcg_manager/entity/record.dart';
 import 'package:tcg_manager/entity/user_data.dart';
+import 'package:tcg_manager/helper/db_helper.dart';
+import 'package:tcg_manager/main.dart';
+import 'package:tcg_manager/provider/deck_list_provider.dart';
 import 'package:tcg_manager/provider/firebase_auth_provider.dart';
+import 'package:tcg_manager/provider/firestore_controller_provider.dart';
+import 'package:tcg_manager/provider/record_list_provider.dart';
+import 'package:tcg_manager/provider/tag_list_provider.dart';
+import 'package:tcg_manager/repository/deck_repository.dart';
+import 'package:tcg_manager/repository/firestore_share_data_repository.dart';
 import 'package:tcg_manager/repository/firestore_share_repository.dart';
 import 'package:tcg_manager/repository/firestore_user_settings_repositroy.dart';
+import 'package:tcg_manager/repository/game_repository.dart';
+import 'package:tcg_manager/repository/record_repository.dart';
+import 'package:tcg_manager/repository/tag_repository.dart';
 import 'package:tcg_manager/view/component/list_tile_ontap.dart';
 import 'package:tcg_manager/view/select_domain_data_bottom_sheet/domain_data_options.dart';
 import 'package:tcg_manager/view/shared_user_view.dart';
@@ -147,7 +163,7 @@ class ShareUserManagementView extends HookConsumerWidget {
                   );
                 },
               ),
-              // TODO 共有を解除するボタン設置
+              const _RevokeShareGameButton(),
             ],
           ),
         );
@@ -187,6 +203,100 @@ class _UserDataSliverList extends StatelessWidget {
               : null,
         );
       },
+    );
+  }
+}
+
+class _RevokeShareGameButton extends HookConsumerWidget {
+  const _RevokeShareGameButton();
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final myself = ref.watch(firebaseAuthNotifierProvider.select((value) => value.user));
+    final currentDataAsyncValue = ref.watch(currentDataProvider);
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 48),
+        child: currentDataAsyncValue.maybeWhen(
+          data: (data) {
+            return InkWell(
+              onTap: () async {
+                if (data.currentShare.authorName == myself?.uid) {
+                  final result = await showOkCancelAlertDialog(
+                    context: context,
+                    title: '共有を解除します',
+                    message: '共有が解除されると他のユーザーはデータが閲覧できなくなります。あなたは引き続き全てのデータを閲覧可能です。',
+                  );
+                  if (result == OkCancelResult.ok) {
+                    final deckList = await ref.read(firestoreShareDataDeckProvider(data.currentShare.docName).future);
+                    final tagList = await ref.read(firestoreShareDataTagProvider(data.currentShare.docName).future);
+                    final recordList = await ref.read(firestoreShareDataRecordProvider(data.currentShare.docName).future);
+                    await ref.read(dbHelper).deleteGame(data.currentShare.game);
+                    await ref.read(gameRepository).insert(data.currentShare.game.copyWith(isShare: false));
+
+                    final Map<int, int> deckIdMap = {};
+                    for (final deck in deckList) {
+                      final newId = await ref.read(deckRepository).insert(deck.copyWith(id: null));
+                      deckIdMap[deck.id!] = newId;
+                    }
+
+                    final Map<int, int> tagIdMap = {};
+                    for (final tag in tagList) {
+                      final newId = await ref.read(tagRepository).insert(tag.copyWith(id: null));
+                      tagIdMap[tag.id!] = newId;
+                    }
+
+                    final List<Record> newRecordList = [];
+                    for (var record in recordList) {
+                      final newUseDeckId = deckIdMap[record.useDeckId];
+                      final newOpponentDeckId = deckIdMap[record.opponentDeckId];
+                      final newTagIdList = record.tagId.map((e) => tagIdMap[e]!).toList();
+                      record = record.copyWith(useDeckId: newUseDeckId, opponentDeckId: newOpponentDeckId, tagId: newTagIdList);
+                      if (record.imagePath != null || record.imagePath!.isNotEmpty) {
+                        final List<String> newImagePath = [];
+                        for (final imagePath in record.imagePath!) {
+                          final response = await http.get(Uri.parse(imagePath));
+                          final saveDir = ref.read(imagePathProvider);
+                          final imageName = imagePath.split('/').last;
+                          final filePath = '$saveDir/$imageName';
+                          final file = File(filePath);
+                          await file.writeAsBytes(response.bodyBytes);
+                          newImagePath.add(filePath);
+                        }
+                        record = record.copyWith(imagePath: newImagePath);
+                      }
+                      newRecordList.add(record);
+                    }
+                    await ref.read(recordRepository).insertList(newRecordList.map((e) => e.copyWith(id: null)).toList());
+                    ref.invalidate(allRecordListProvider);
+                    ref.invalidate(allDeckListProvider);
+                    ref.invalidate(allTagListProvider);
+                    await ref.read(firestoreControllerProvider).deleteShareGame(data.currentShare.docName);
+                    if (context.mounted) {
+                      Navigator.of(context).pop();
+                    }
+                  }
+                } else {
+                  await showOkAlertDialog(
+                    context: context,
+                    title: '権限がありません。',
+                    message: 'データの削除はこのゲームの作成者のみ許可されています。',
+                  );
+                }
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Center(
+                  child: Text(
+                    '共有を解除',
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.error),
+                  ),
+                ),
+              ),
+            );
+          },
+          orElse: () => Container(),
+        ),
+      ),
     );
   }
 }
