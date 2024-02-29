d0a8aced94314a0ca9bfe004ffa6766b7ccf2591
taichi1001 <t.sasaki178@gmail.com>
2024-03-01 02:15:52 +0900

    ストアのURLをトレマネのものに設定

--- lib/helper/review_helper.dart
+++ lib/helper/review_helper.dart
@@ -8,10 +8,9 @@ class ReviewHelper {
   static final InAppReview _inAppReview = InAppReview.instance;
 
   // URLを定数化
-  // TODO トレマネのURLに変更する
   static const String _urlAppStore =
-      'https://apps.apple.com/jp/app/%E3%83%88%E3%83%AC%E3%82%AB%E7%A2%BA%E7%8E%87%E3%82%B7%E3%83%9F%E3%83%A5%E3%83%AC%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%82%A2%E3%83%97%E3%83%AA-%E3%83%88%E3%83%AC%E3%82%B7%E3%83%9F%E3%83%A5/id6475571696?ign-mpt=uo%3D4';
-  static const String _urlPlayStore = 'https://play.google.com/store/apps/details?id=com.taichi1001.tcg_calc';
+      'https://apps.apple.com/jp/app/%E3%83%88%E3%83%AC%E3%82%AB%E6%88%A6%E7%B8%BE%E7%AE%A1%E7%90%86%E3%82%A2%E3%83%97%E3%83%AA-%E3%83%88%E3%83%AC%E3%83%9E%E3%83%8D/id1609073371';
+  static const String _urlPlayStore = 'https://play.google.com/store/apps/details?id=com.taichi1001.tcg_manager';
 
   static void launchStoreReview(BuildContext context) async {
     // ScaffoldMessengerのBuildContextを保存
